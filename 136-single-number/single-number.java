82a5a3ff49e60c00ae971ba3829c24253b107814
Varun <varunkumary001@gmail.com>
2025-10-05 20:15:19 +0530

    Time: 1 ms (99.87%) | Memory: 46.5 MB (18.53%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,20 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        int result = 0;
         for(int i=0;i<nums.length;i++){
-            if(!hm.containsKey(nums[i])){
-                hm.put(nums[i],1);
-            }else{
-                int temp = hm.get(nums[i]);
-                hm.put(nums[i],temp+1);
-            }
-        }
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            if (entry.getValue() == 1) { // Check if the count (value) is 1
-                return entry.getKey();   // Return the number (key)
-            }
-        
-                }
-                throw new IllegalArgumentException("No single number found in the array.");
-                }
+        result = result ^ nums[i];
+    }
+    return result;
+}
 }
