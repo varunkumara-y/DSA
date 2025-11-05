bc66276f109df324d5384caa597bf97fdc04fabc
Varun <varunkumary001@gmail.com>
2025-11-05 19:24:03 +0530

    Time: 1325 ms (6.25%) | Memory: 195.7 MB (6.25%) - LeetSync

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.java
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.java
@@ -1,57 +1,53 @@
 class Solution {
-    private int x;
-    private long sum = 0L;
-    private Map<Integer, Integer> freq;
-    private final TreeSet<int[]> active = new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]), inactive = new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-    public long[] findXSum(int[] nums, int k, int x) {
-        int n = nums.length;
-        this.x = x;
-        freq = new HashMap<>(n);
-        long[] ans = new long[n - k + 1];
-        for(int i = 0; i < n; i++) {
-            int count = freq.merge(nums[i], 1, Integer::sum);
-            remove(count - 1, nums[i]);
-            add(count, nums[i]);
-            if(i + 1 >= k) {
-                ans[i - k + 1] = sum;
-                count = freq.merge(nums[i - k + 1], -1, Integer::sum);
-                remove(count + 1, nums[i - k + 1]);
-                add(count, nums[i - k + 1]);
-            }
-        }
-        return ans;
-    }
-    private void add(int count, int num) {
-        if(count == 0) return;
-        int[] val = new int[] {count, num};
-        if(active.size() < x) {
-            active.add(val);
-            sum += (long)count * num;
-            return;
-        }
-        int[] temp = active.first();
-        if(temp[0] > count || temp[0] == count && temp[1] >= num) {
-            inactive.add(val);
-            return;
-        }
-        sum += (long)count * num - (long)temp[0] * temp[1];
-        inactive.add(active.pollFirst());
-        active.add(val);
-    }
-    private void remove(int count, int num) {
-        if(count == 0) return;
-        int[] val = new int[] {count, num};
-        if(inactive.contains(val)) {
-            inactive.remove(val);
-            return;
-        }
-        
-        active.remove(val);
-        sum -= (long)count * num;
-        if(inactive.isEmpty()) return;
+  public long[] findXSum(int[] nums, int k, int x) {
+    long[] ans = new long[nums.length - k + 1];
+    Map<Integer, Integer> count = new HashMap<>();
+    TreeSet<Pair<Integer, Integer>> top =
+        new TreeSet<>(Comparator.comparingInt(Pair<Integer, Integer>::getKey)
+                          .thenComparingInt(Pair<Integer, Integer>::getValue));
+    TreeSet<Pair<Integer, Integer>> bot =
+        new TreeSet<>(Comparator.comparingInt(Pair<Integer, Integer>::getKey)
+                          .thenComparingInt(Pair<Integer, Integer>::getValue));
 
-        int[] temp = inactive.pollLast();
-        sum += (long)temp[0] * temp[1];
-        active.add(temp);
+    for (int i = 0; i < nums.length; ++i) {
+      update(nums[i], 1, count, top, bot);
+      if (i >= k)
+        update(nums[i - k], -1, count, top, bot);
+      while (!bot.isEmpty() && top.size() < x) {
+        Pair<Integer, Integer> pair = bot.pollLast();
+        top.add(pair);
+        windowSum += (long) pair.getValue() * pair.getKey();
+      }
+      while (!bot.isEmpty() && (bot.last().getKey() > top.first().getKey() ||
+                                bot.last().getKey().equals(top.first().getKey()) &&
+                                    bot.last().getValue() > top.first().getValue())) {
+        Pair<Integer, Integer> pairB = bot.pollLast();
+        Pair<Integer, Integer> pairT = top.pollFirst();
+        top.add(pairB);
+        bot.add(pairT);
+        windowSum += (long) pairB.getValue() * pairB.getKey();
+        windowSum -= (long) pairT.getValue() * pairT.getKey();
+      }
+      if (i >= k - 1)
+        ans[i - k + 1] = windowSum;
     }
+    return ans;
+  }
+
+  private long windowSum = 0;
+
+  private void update(int num, int freq, Map<Integer, Integer> count,
+                      TreeSet<Pair<Integer, Integer>> top, TreeSet<Pair<Integer, Integer>> bot) {
+    if (count.getOrDefault(num, 0) > 0) { 
+      Pair<Integer, Integer> pair = new Pair<>(count.get(num), num);
+      if (bot.remove(pair)) {
+      } else {
+        top.remove(pair);
+        windowSum -= (long) num * count.get(num);
+      }
+    }
+    count.merge(num, freq, Integer::sum);
+    if (count.get(num) > 0)
+      bot.add(new Pair<>(count.get(num), num));
+  }
 }
