c81568b3820694ae33ff31ba44add6844b95d84b
Varun <varunkumary001@gmail.com>
2025-12-24 21:00:45 +0530

    Time: 4 ms (82.79%) | Memory: 44.9 MB (5.34%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minimumBoxes(int[] apple, int[] capacity) {
-        int m = capacity.length;
         int sum=0;
-        int count=0;
         for(int s:apple){
             sum+=s;
         }
        
         Arrays.sort(capacity);
+        int count=0;
+        int m = capacity.length;
         for(int i=m-1;i>=0;i--){
                 sum-=capacity[i];
                 count++;
